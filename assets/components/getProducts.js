481206422622a8fca16db37f043616d0a3cbb8bc
Adi <121740959+Adi020@users.noreply.github.com>
2024-01-18 00:34:04 -0600

    Update getProducts.js

--- assets/components/getProducts.js
+++ assets/components/getProducts.js
@@ -7,19 +7,19 @@ async function getProducts () {
 //        console.log(error)
 // }
     return [
-    {id:1, name:'hoodie', "quantity": 14, price:25.00, image:'https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format'},
-    {"id":2, "name":'camisa', "quantity": 1, "price":20.00, "image":'https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format'},
-    {"id":3, "name":'camisa', "quantity": 3, "price":30.00, "image":'https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format'},
-    {"id":4, "name":'hoodie', "quantity": 4, "price":10.00, "image":'https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format'},
-    {"id":5, "name":'sudadera', "quantity": 88, "price":25.00, "image":'https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format'},
-    {"id":6, "name":'hoodie', "quantity": 1, "price":5.00, "image":'https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format'},
-    {"id":7, "name":'sudadera', "quantity": 0, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":8, "name":'camisa', "quantity": 4, "price":22.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":9, "name":'hoodie', "quantity": 6, "price":30.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":10, "name":'sudadera', "quantity": 67, "price":'25.00', "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":11, "name":'hoodie', "quantity": 14, "price":19.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":12, "name":'camisa', "quantity": 14, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":13, "name":'sudadera', "quantity": 44, "price":10.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'}
+    {id:1, name:'hoodie', "quantity":14, price:25.00, image:'https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format'},
+    {"id":2, "name":'camisa', "quantity":1, "price":20.00, "image":'https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format'},
+    {"id":3, "name":'camisa', "quantity":3, "price":30.00, "image":'https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format'},
+    {"id":4, "name":'hoodie', "quantity":4, "price":10.00, "image":'https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format'},
+    {"id":5, "name":'sudadera', "quantity":88, "price":25.00, "image":'https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format'},
+    {"id":6, "name":'hoodie', "quantity":1, "price":5.00, "image":'https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format'},
+    {"id":7, "name":'sudadera', "quantity":0, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":8, "name":'camisa', "quantity":4, "price":22.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":9, "name":'hoodie', "quantity":6, "price":30.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":10, "name":'sudadera', "quantity":67, "price":'25.00', "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":11, "name":'hoodie', "quantity":14, "price":19.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":12, "name":'camisa', "quantity":14, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
+    {"id":13, "name":'sudadera', "quantity":44, "price":10.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'}
 
 ]
 }
