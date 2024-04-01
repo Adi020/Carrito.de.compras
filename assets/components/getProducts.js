b037a0ab7139b4b5eb9b813be03bcf9c801abf45
Adi <121740959+Adi020@users.noreply.github.com>
2024-03-31 18:02:31 -0600

    Update getProducts.js

--- assets/components/getProducts.js
+++ assets/components/getProducts.js
@@ -1,27 +1,131 @@
-async function getProducts () {
-//    try {
-//        const res = await window.fetch("https://ecommercebackend.fundamentos-29.repl.co/")
-//        const data = await res.json()
-//        return data
-//    } catch (error) {
-//        console.log(error)
-// }
-    return [
-    {id:1, name:'hoodie', quantity:14, price:25.00, image:'https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format'},
-    {"id":2, "name":'camisa', "quantity":1, "price":20.00, "image":'https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format'},
-    {"id":3, "name":'camisa', "quantity":3, "price":30.00, "image":'https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format'},
-    {"id":4, "name":'hoodie', "quantity":4, "price":10.00, "image":'https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format'},
-    {"id":5, "name":'sudadera', "quantity":88, "price":25.00, "image":'https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format'},
-    {"id":6, "name":'hoodie', "quantity":1, "price":5.00, "image":'https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format'},
-    {"id":7, "name":'sudadera', "quantity":0, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":8, "name":'camisa', "quantity":4, "price":22.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":9, "name":'hoodie', "quantity":6, "price":30.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":10, "name":'sudadera', "quantity":67, "price":'25.00', "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":11, "name":'hoodie', "quantity":14, "price":19.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":12, "name":'camisa', "quantity":14, "price":8.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'},
-    {"id":13, "name":'sudadera', "quantity":44, "price":10.00, "image":'https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format'}
-
-]
+async function getProducts() {
+  //    try {
+  //        const res = await window.fetch("https://ecommercebackend.fundamentos-29.repl.co/")
+  //        const data = await res.json()
+  //        return data
+  //    } catch (error) {
+  //        console.log(error)
+  // }
+  return [
+    {
+      id: 1,
+      name: "Hoddie con capucha y cordon ajustable",
+      quantity: 0,
+      price: 10,
+      image:
+        "https://img.represent.com/uploads/97cc04c33fb623827703747d01496782.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 2,
+      name: "Camiseta de manga corta con cuello redondo",
+      quantity: 1,
+      price: 20.0,
+      image:
+        "https://img.represent.com/uploads/a76857819caae151f62fb58e05621acd.jpeg?w=750&q=95&auto=format",
+      description:
+        "Esta camiseta basica presenta un corte regular y un cuello redondo clásico. Es ideal para el uso diario y se puede combiar con una alia variedad de looks.",
+    },
+    {
+      id: 3,
+      name: "Camiseta de manga corta con cuello redondo",
+      quantity: 3,
+      price: 30.0,
+      image:
+        "https://img.represent.com/uploads/e33ba02f02bf3f18ba5e0db1a424ba56.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 4,
+      name: "Hoddie con capucha y cordon ajustable",
+      quantity: 4,
+      price: 10.0,
+      image:
+        "https://img.represent.com/uploads/535666bf499a0c508ace882bb0420436.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 5,
+      name: "Sweater de punto grueso",
+      quantity: 88,
+      price: 25.0,
+      image:
+        "https://img.represent.com/uploads/ee4a165cfdce4d17714bf8a39211746b.jpeg?w=750&q=95&auto=format",
+      description:
+        "Perfecta para un look casual, esta camiseta de manga larga presenta un estampado gráfico llamativo en el pecho. su ajuste regular y suave tejido de algodon la hacen comoda",
+    },
+    {
+      id: 6,
+      name: "Hoddie con capucha y cordon ajustable",
+      quantity: 1,
+      price: 5.0,
+      image:
+        "https://img.represent.com/uploads/46ac9d39acbd33cee2aadd5b6c230dc0.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 7,
+      name: "Sweater de punto grueso",
+      quantity: 0,
+      price: 8.0,
+      image:
+        "https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 8,
+      name: "Sweater de punto grueso",
+      quantity: 4,
+      price: 22.0,
+      image:
+        "https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 9,
+      name: "Hoddie con capucha y cordon ajustable",
+      quantity: 6,
+      price: 30.0,
+      image:
+        "https://d2v48i7nl75u94.cloudfront.net/uploads/730128eb13dea51da33f3f7e5946ee3d.jpeg",
+    },
+    {
+      id: 10,
+      name: "Sweater de punto grueso",
+      quantity: 67,
+      price: "25.00",
+      image:
+        "https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 11,
+      name: "Sweater de punto grueso",
+      quantity: 14,
+      price: 19.0,
+      image:
+        "https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 12,
+      name: "Sweater de punto grueso",
+      quantity: 14,
+      price: 8.0,
+      image:
+        "https://img.merch.cameo.com/uploads/97cc04c33fb623827703747d01496782.jpeg?q=95&auto=format",
+    },
+    {
+      id: 13,
+      name: "Sweater de punto grueso",
+      quantity: 44,
+      price: 10.0,
+      image:
+        "https://img.represent.com/uploads/39eecc78c3a4e8bcf0542f9088d0df61.jpeg?w=750&q=95&auto=format",
+    },
+    {
+      id: 14,
+      name: "Camiseta de manga corta con cuello redondo",
+      quantity: 1,
+      price: 20.0,
+      image:
+        "    https://img.represent.com/uploads/60d0c0fcd09e31fbc3d64b6159fcb05e.jpeg?w=750&q=95&auto=format",
+      description:
+        "Esta camiseta basica presenta un corte regular y un cuello redondo clásico. Es ideal para el uso diario y se puede combiar con una alia variedad de looks.",
+    },
+  ];
 }
 
-export default getProducts
+export default getProducts;
